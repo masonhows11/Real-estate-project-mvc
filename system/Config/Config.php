d22884c5330d0356bd4dc6eea07d35bfcd35b2b0
mason.hows11 <mason.hows11@gmail.com>
2025-04-13 21:23:03 +0330

    config updated

--- system/Config/Config.php
+++ system/Config/Config.php
@@ -9,10 +9,8 @@ class Config
 
 
     private static $instance;
-
     private array $config_nested_array = [];
     private array $config_dot_array = [];
-
 
 
     /**
@@ -27,65 +25,59 @@ class Config
     private function initialConfigArrays(): void
     {
 
-         // dirname -> Returns a parent directory's path
-
-        $configPath = dirname(dirname(__DIR__)).'/config/';
+        // dirname -> Returns a parent directory's path
+        // $configPath = dirname(dirname(__DIR__)).'/config/';
+        $configPath = dirname(__DIR__, 2) . '/config/';
 
         // read all config php files from config dir as array
-        foreach (glob($configPath.'*.php') as $filename)
-        {
-            // get file content and put into $config var
+        foreach (glob($configPath . '*.php') as $filename) {
+            
+            // get all file  contents in config dir and put into $config var
             $config = require $filename;
             $key = $filename;
-            $key = str_replace($configPath,"",$key);
-            $key = str_replace('.php',"",$key);
+            $key = str_replace($configPath, "", $key);
+            $key = str_replace('.php', "", $key);
 
             $this->config_nested_array[$key] = $config;
         }
         $this->initialDefaultValues(); // get current route
         $this->config_dot_array = $this->array_dot($this->config_nested_array);
-
     }
 
     private function array_dot($array, $return_array = array(), $return_key = ''): array
     {
         // make config item with dot like
         // app.app_title or mail.SMTP.host
-        foreach ($array as $key => $value){
-            if(is_array($value))
-            {
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
                 // if we have nested array
                 // we call array_dot like recursive method
-                $return_array = array_merge($return_array,$this->array_dot($value,$return_array,$return_key.$key.'.'));
-            }else{
+                $return_array = array_merge($return_array, $this->array_dot($value, $return_array, $return_key . $key . '.'));
+            } else {
                 // example  ['APP_TITLE'] => 'mvc project',
                 $return_array[$return_key . $key] = $value;
             }
         }
-        return  $return_array;
+        return $return_array;
     }
 
+
+    // get current route
     private function initialDefaultValues(): void
     {
-
-        // get current route
-        $temporary = str_replace($this->config_nested_array['app']['BASE_URL'],'',explode('?',$_SERVER['REQUEST_URI'])[0]);
-        $temporary === "/" ? $temporary = "" : $temporary = substr($temporary,1);
+        $temporary = str_replace($this->config_nested_array['app']['BASE_URL'], '', explode('?', $_SERVER['REQUEST_URI'])[0]);
+        $temporary === "/" ? $temporary = "" : $temporary = substr($temporary, 1);
         // make current_route & add into app file in array
         $this->config_nested_array['app']['CURRENT_ROUTE'] = $temporary;
     }
 
 
-
-
     private static function getInstance(): Config
     {
-            if(empty(self::$instance))
-            {
-                self::$instance = new self;
-            }
-
-            return self::$instance;
+        if (empty(self::$instance)) {
+            self::$instance = new self;
+        }
+        return self::$instance;
     }
 
 
@@ -95,15 +87,12 @@ class Config
     public static function get($key)
     {
         $instance = self::getInstance();
-        if(isset($instance->config_dot_array[$key])){
-
+        if (isset($instance->config_dot_array[$key])) {
             return $instance->config_dot_array[$key];
-
-        }else{
-
-            throw new Exception('"'. $key .'" not exist in config ');
+        } else {
+            throw new Exception('"' . $key . '" not exist in config ');
         }
     }
-    
+
 
 }
