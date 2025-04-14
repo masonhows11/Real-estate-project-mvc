2fc1d5134bb2d74a25347fe840143c2e4e456725
mason.hows11 <mason.hows11@gmail.com>
2025-04-14 20:33:47 +0330

    InlcudeContent updated

--- system/View/Traits/IncludeContent.php
+++ system/View/Traits/IncludeContent.php
@@ -13,6 +13,7 @@ trait IncludeContent
      */
     private function checkIncludesContent(): void
     {
+        // $includesNamesArray -> there may be multiple @include in view
         // this while(1) for while we have nested includes
         while (1) {
             $includesNamesArray = $this->findIncludesNames();
@@ -26,10 +27,13 @@ trait IncludeContent
         }
     }
 
+
+    // find @includes in child view
     private function findIncludesNames(): null|false
     {
         $includesNamesArray = [];
         // to use extends method or not
+        // $this->content is content of child view
         preg_match("/@include+\('([^)]+)'\)/", $this->content, $includesNamesArray);
         // return isset($includesNamesArray[1]) ? $includesNamesArray[1] : false;
         return $includesNamesArray[1] ?? false;
@@ -38,10 +42,12 @@ trait IncludeContent
 
     /**
      * @throws Exception
+     * fill @include in child view
      */
     private function initialIncludes($includeName): array|string
     {
         // @include('views.404')
+        // $this->content is content of child view
         return $this->content = str_replace("@include('$includeName')", $this->viewLoader($includeName), $this->content);
     }
 }
