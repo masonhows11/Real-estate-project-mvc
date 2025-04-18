cff0d1e7f163cad4eaeb58c573a744d96ec8ac35
mason.hows11 <mason.hows11@gmail.com>
2025-04-18 22:08:25 +0330

    load css js files update3d

--- resources/view/admin/layouts/head.blade.php
+++ resources/view/admin/layouts/head.blade.php
@@ -5,37 +5,36 @@
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
 
-    <link rel="apple-touch-icon" href="<?=  asset('app-assets/images/ico/apple-icon-120.png') ?>">
-    <link rel="shortcut icon" type="image/x-icon" href="<?= asset('app-assets/images/ico/favicon.ico') ?>">
+    <link rel="apple-touch-icon" href="<?=  asset('admin_assets/images/ico/apple-icon-120.png') ?>">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= asset('admin_assets/images/ico/favicon.ico') ?>">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="<? asset('app-assets/vendors/css/vendors-rtl.min.css') ?>">
-    <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/vendors/css/charts/apexcharts.css') ?>">
-    <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/vendors/css/extensions/toastr.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/vendors/css/vendors-rtl.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/vendors/css/charts/apexcharts.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/vendors/css/extensions/toastr.min.css') ?>">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/bootstrap.css') ?>">
-    <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/bootstrap-extended.css') ?>">
-    <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/colors.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?=  asset('app-assets/css-rtl/components.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/themes/dark-layout.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/themes/bordered-layout.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/themes/semi-dark-layout.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/bootstrap.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/bootstrap-extended.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/colors.css') ?>">
+   <link rel="stylesheet" type="text/css" href="<?=  asset('admin_assets/css-rtl/components.css') ?>">
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/themes/dark-layout.css') ?>">
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/themes/bordered-layout.css') ?>">
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/themes/semi-dark-layout.css') ?>">
 
    <!-- BEGIN: Page CSS-->
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/core/menu/menu-types/vertical-menu.css') ?>">
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/pages/dashboard-ecommerce.css') ?>">
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/plugins/charts/chart-apex.css') ?>">
    <link rel="stylesheet" type="text/css"
-         href="<?= asset('app-assets/css-rtl/core/menu/menu-types/vertical-menu.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/pages/dashboard-ecommerce.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/plugins/charts/chart-apex.css') ?>">
-   <link rel="stylesheet" type="text/css"
-         href="<?= asset('app-assets/css-rtl/plugins/extensions/ext-component-toastr.css') ?>">
+         href="<?= asset('admin_assets/css-rtl/plugins/extensions/ext-component-toastr.css') ?>">
    <!-- END: Page CSS-->
 
    <!-- BEGIN: Custom CSS-->
-   <link rel="stylesheet" type="text/css" href="<?= asset('app-assets/css-rtl/custom-rtl.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?= asset('assets/css/style-rtl.css') => ">
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/css-rtl/custom-rtl.css') ?>">
+   <link rel="stylesheet" type="text/css" href="<?= asset('admin_assets/assets/css/style-rtl.css') ?>">
     <!-- END: Custom CSS-->
 
 
