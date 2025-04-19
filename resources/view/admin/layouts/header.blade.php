67478a052d55a15706499d04e4dcf041cb29fcbf
mason.hows11 <mason.hows11@gmail.com>
2025-04-19 21:15:18 +0330

    determin admin section in html header admin panel

--- resources/view/admin/layouts/header.blade.php
+++ resources/view/admin/layouts/header.blade.php
@@ -199,17 +199,18 @@
                 </ul>
             </li>
             <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <!-- start user section -->
                     <div class="user-nav d-sm-flex d-none">
                        <span class="user-name fw-bolder">
 
-
-
                        </span>
                         <span class="user-status">Admin</span>
-                    </div><span class="avatar">
+                    </div>
+                    <span class="avatar">
                         <img class="round" src="" alt="avatar" height="40" width="40">
                         <span class="avatar-status-online"></span>
                     </span>
+                    <!-- end user section -->
                 </a>
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
                     <a class="dropdown-item" href="#"><i class="me-50" data-feather="user"></i> Profile</a>
