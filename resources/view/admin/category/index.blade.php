1ef51b3c9b5336fb6d5669bde01916a212dda005
mason.hows11 <mason.hows11@gmail.com>
2025-04-23 09:58:38 +0330

    admin category show category list table html updated

--- resources/view/admin/category/index.blade.php
+++ resources/view/admin/category/index.blade.php
@@ -7,7 +7,23 @@
 @section('main_content')
     <div class="container border-2 rounded border-primary">
 
-        <h1 class="my-4">دسته بندی ها</h1>
+        <h1 class="my-1">دسته بندی ها</h1>
+        <div class="my-2">
+            <a class="btn btn-success" href="<?= route('admin.category.create') ?>">ایجاد</a>
+            <div class="row">
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-column p-1">
+                        <label for="show">تعداد</label>
+                        <input id="show" class="form-control mt-1" type="text">
+                    </div>
+                    <div class="d-flex flex-column p-1">
+                        <label for="show">جستجو</label>
+                        <input id="show" class="form-control mt-1" type="text">
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
         <!--<ul>
             <?php foreach ($categories as $category): ?>
