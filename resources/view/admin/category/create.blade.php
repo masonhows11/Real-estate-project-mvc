9d3ba7a103aa83564bd844b72c9ee388b50ea36c
mason.hows11 <mason.hows11@gmail.com>
2025-04-25 17:03:38 +0330

    create cat add view template updated

--- resources/view/admin/category/create.blade.php
+++ resources/view/admin/category/create.blade.php
@@ -7,7 +7,23 @@
 @section('main_content')
     <div class="container border-2 rounded border-primary">
 
-        <h1 class="my-4"> دسته بندی جدید </h1>
+        <div class="row px-2">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <h1 class="my-1">دسته بندی جدید</h1>
+                </div>
+                <div>
+                   <h4 class="" style="margin-top: 16px"><a class="" href="<?= route('admin.category.index') ?>">دسته بندی ها</a></h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="row px-2">
+
+        </div>
+
+
+
 
     </div>
 @endsection
