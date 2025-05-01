bbab3f21012f9bd6317fb2658bdd751bbf9d368a
mason.hows11 <mason.hows11@gmail.com>
2025-05-01 13:53:22 +0330

    add enctype for create post

--- resources/view/admin/category/create.blade.php
+++ resources/view/admin/category/create.blade.php
@@ -13,18 +13,20 @@
                     <h1 class="my-1">دسته بندی جدید</h1>
                 </div>
                 <div>
-                   <h4 class="" style="margin-top: 18px"><a class="" href="<?= route('admin.category.index') ?>">دسته بندی ها</a></h4>
+                    <h4 class="" style="margin-top: 18px"><a class=""
+                            href="<?= route('admin.category.index') ?>">دسته بندی ها</a></h4>
                 </div>
             </div>
         </div>
 
         <div class="row my-2">
-            <form action="<?= route('admin.category.store') ?>" method="post">
+            <form action="<?= route('admin.category.store') ?>" method="post" enctype="multipart/form-data">
 
                 <div class="mb-3 mt-3">
                     <label for="category" class="form-label">نام:</label>
-                    <input type="text" class="form-control <?= errorClass('name') ?> " id="category" placeholder="نام دسته بندی را وارد کنید" name="name" value="<?= old('name') ?>">
-                    <?= errorText('name')  ?>
+                    <input type="text" class="form-control <?= errorClass('name') ?> " id="category"
+                        placeholder="نام دسته بندی را وارد کنید" name="name" value="<?= old('name') ?>">
+                    <?= errorText('name') ?>
                 </div>
 
                 <div class="mb-3">
@@ -32,10 +34,11 @@
                     <select id="parent" class="form-select <?= errorClass('parent_id') ?>" name="parent_id">
                         <option value="">در صورت نیاز دسته والد را انتخاب کنید</option>
                         <?php foreach ($categories as $category){ ?>
-                        <option <?= old('parent_id') == $category->id ? 'selected' : '' ?> value="<?= $category->id ?>"><?= $category->name ?></option>
+                        <option <?= old('parent_id') == $category->id ? 'selected' : '' ?> value="<?= $category->id ?>">
+                            <?= $category->name ?></option>
                         <?php } ?>
                     </select>
-                    <?= errorText('name')  ?>
+                    <?= errorText('name') ?>
                 </div>
 
                 <button type="submit" class="btn btn-primary">ذخیره</button>
@@ -50,5 +53,4 @@
 
 
 @section('scripts')
-
 @endsection
