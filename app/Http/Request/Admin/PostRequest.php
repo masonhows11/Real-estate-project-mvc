c40e49ab9717fd2c5952200df0f8d17427fb781c
mason.hows11 <mason.hows11@gmail.com>
2025-05-02 20:37:23 +0330

    validate request post updated

--- app/Http/Request/Admin/PostRequest.php
+++ app/Http/Request/Admin/PostRequest.php
@@ -4,18 +4,34 @@ namespace App\Http\Request\Admin;
 
 use System\Request\Request;
 
-class PostRequest extends  Request
+class PostRequest extends Request
 {
     public function rules(): array
     {
-        return [
+        // for update post
+        if (methodField() == 'put')
+        {
+            return [
 
-            'title' => "required|min:5|max:127",
-            'body' => "required",
-            'cat_id' => "required|exists:categories,id",
-            'image' => "required|file|mimes:jpeg,jpg,png,gif",
-            'published_at' => "required|date",
+                'title' => "required|min:5|max:127",
+                'body' => "required",
+                'cat_id' => "required|exists:categories,id",
+                'image' => "file|mimes:jpeg,jpg,png,gif",
+                'published_at' => "required|date",
 
-        ];
+            ];
+        } else {
+            // for new post
+            return [
+
+                'title' => "required|min:5|max:127",
+                'body' => "required",
+                'cat_id' => "required|exists:categories,id",
+                'image' => "required|file|mimes:jpeg,jpg,png,gif",
+                'published_at' => "required|date",
+
+            ];
+        }
+
     }
 }
---
+++ app/Http/Request/Admin/PostUpdateRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Request\Admin;
+
+use System\Request\Request;
+
+class PostUpdateRequest extends Request
+{
+
+    public function rules(): array
+    {
+        return [
+
+            'title' => "required|min:5|max:127",
+            'body' => "required",
+            'cat_id' => "required|exists:categories,id",
+            'image' => "required|file|mimes:jpeg,jpg,png,gif",
+            'published_at' => "required|date",
+
+        ];
+    }
+}
