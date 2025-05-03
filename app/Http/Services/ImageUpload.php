1fd0f9ba53259f7293a4db58bda2c94349588ade
mason.hows11 <mason.hows11@gmail.com>
2025-05-03 21:21:23 +0330

    image uploader class implements updated

--- app/Http/Services/ImageUpload.php
+++ app/Http/Services/ImageUpload.php
@@ -3,11 +3,31 @@
 namespace App\Http\Services;
 
 use Intervention\Image\ImageManager;
+
 class ImageUpload
 {
 
-    public static function uploadAndFitImage(mixed $file, string $path, string $image_name, int $int, int $int1)
+    public static function uploadAndFitImage(mixed $file, string $path, string $image_name, int $width, int $height)
     {
+
+
+        $path = trim('\/') . "/";
+        $image_name = trim('\/') . '.' . pathinfo($file['name'], PATHINFO_EXTENSION);
+
+        if (!is_dir($path))
+        {
+            if (!mkdir($path, 0777, true))
+            {
+                die("image upload : failed create directory");
+            };
+        }
+        is_writable($path);
+
+            $manager  = new ImageManager(array('driver' => 'GD'));
+            $image = $manager->make($file['temp_name'])->fit($width,$height);
+            $image->save($path.$image_name);
+            return '/'.$path.$image_name;
+
 
     }
 }
