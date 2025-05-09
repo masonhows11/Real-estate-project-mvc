ee1f84d5dacd9c1235269f3a44385ec0607965bc
mason.hows11 <mason.hows11@gmail.com>
2025-05-09 10:44:01 +0330

    ads request updated

--- app/Http/Request/Admin/AdsRequest.php
+++ app/Http/Request/Admin/AdsRequest.php
@@ -8,31 +8,55 @@ class AdsRequest extends Request
 {
     public function rules(): array
     {
-        return [
 
-            'title' => "required|min:5|max:127",
-            'description' => "required|min:10",
-            'address' => "required|min:10|max:250",
-            'amount' => "required",
-            'cat_id' => "required|exists:categories,id",
-            'image' => "required|file|mimes:jpeg,jpg,png,gif",
-            'floor' => "required",
-            'year' => "required",
-            'storeroom' => "required",
-            'balcony' => "required",
-            'area' => "required",
-            'room' => "required",
-            'toilet' => "required",
-            'parking' => "required",
-            'tag' => "required",
-            'status' => "required",
-            'sell_status' => "required",
-            'type' => "required",
-            //'view' => "",
+        if (methodField() == 'put') {
 
+            return [
 
+                'title' => "required|min:5|max:127",
+                'description' => "required|min:10|max:1000",
+                'address' => "required|min:10|max:250",
+                'amount' => "required",
+                'cat_id' => "required|exists:categories,id",
+                'image' => "file|mimes:jpeg,jpg,png,gif",
+                'floor' => "required|max:127",
+                'year' => "required|number",
+                'storeroom' => "required|number",
+                'balcony' => "required|number",
+                'area' => "required|number",
+                'room' => "required|number",
+                'toilet' => "required|number",
+                'parking' => "required|number",
+                'tag' => "required|max:191",
+                'status' => "required",
+                'sell_status' => "required|number",
+                'type' => "required|type",
+              
+            ];
 
-        ];
+        } else {
 
+            return [
+                'title' => "required|min:5|max:127",
+                'description' => "required|min:10|max:1000",
+                'address' => "required|min:10|max:250",
+                'amount' => "required",
+                'cat_id' => "required|exists:categories,id",
+                'image' => "required|file|mimes:jpeg,jpg,png,gif",
+                'floor' => "required|max:127",
+                'year' => "required|number",
+                'storeroom' => "required|number",
+                'balcony' => "required|number",
+                'area' => "required|number",
+                'room' => "required|number",
+                'toilet' => "required|number",
+                'parking' => "required|number",
+                'tag' => "required|max:191",
+                'status' => "required",
+                'sell_status' => "required|number",
+                'type' => "required|type",
+              
+            ];
+        }
     }
 }
