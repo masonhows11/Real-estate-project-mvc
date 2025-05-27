07be6cd8337214758a32f8fd237c0d360673f8bf
mason.hows11 <mason.hows11@gmail.com>
2025-05-27 11:00:58 +0330

    forgot pass method implement updated

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -7,12 +7,13 @@ use App\Models\User;
 use App\Http\Services\MailService;
 use App\Http\Request\ForgotRequest;
 use System\Session\Session;
+
 class ForgotPasswordController
 {
 
 
     private const string home = '/home';
-    
+
     public function forgotView()
     {
         return view('auth.forgot');
@@ -20,7 +21,22 @@ class ForgotPasswordController
 
     public function forgotPassword($token)
     {
-        dd($token);
+        // dd($token);
+
+        if (Session::get('forgot.time') != false && Session::get('forgot.time') > time()) {
+
+            error('forgot_pass_token', 'please wait for 2 min then try again');
+
+        } else {
+
+            Session::set('forgot.time', time() + 120);
+
+            $req = new ForgotRequest();
+            $inputs = $req->all();
+
+            $user = User::where('email',$inputs['email'])->get();
+
+        }
     }
 
 
