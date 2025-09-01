d4768c73e47197d95cd88f39af6e676d0ee729c4
mason.hows11 <mason.hows11@gmail.com>
2025-09-01 07:51:45 +0330

    paginate updated 80%

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -31,7 +31,32 @@ function echoHtml($data): string
     return html_entity_decode($data);
 }
 
-function paginate($data,$per_page = 10, $page = 1, $options = [])
+function paginate($data, $per_page = 10, $page = 1, $options = [])
 {
-        
+
+    $totalRows = count($data);
+    $currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    $totalPages = ceil($totalRows) / $per_page;
+    // current page shouldn't be greater than total page
+    $currentPage = min($currentPage, $totalPages);
+
+    $currentPage = max($currentPage, 1);
+    // example
+    // totalRows 6
+    // current = 2
+    // 2 - 1 * per_page 2 = 2
+    // start from array index 2 to display record on next page in this example page 2
+    //
+    // [0->red,1->green,2->blue,3->white,4->purple,5->black]
+    // get blue as first record for page 2 to display
+    // example current = 3 & per_page = 2
+    // 3 - 1 * 2 = 4
+    // take record at index 4 -> purple in page 3 start from  record purple
+    $currentRow = ($currentPage - 1) * $per_page;
+
+    // add data from start(current row) until per_page
+    // 4 5 ->  2 record
+    $data = array_slice($data, $currentRow, $per_page);
+    return $data;
+
 }
