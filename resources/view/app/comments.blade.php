587f9d1f978fe836750787328a16803a989da0fe
mason.hows11 <mason.hows11@gmail.com>
2025-09-03 10:28:54 +0330

    comments post updated

--- resources/view/app/comments.blade.php
+++ resources/view/app/comments.blade.php
@@ -3,18 +3,22 @@
         <div class="homec-blog-comments">
             <h2 class="homec-blog-detail__title">03 Comments</h2>
 
+            <?php foreach ($comments as $comment){ ?>
             <div class="homec-blog-comments__single">
-                <img src="<?= asset('app_assets/img/blog-comment1.png') ?>" alt="#" />
+                <img src="<?= $comment->user()->avatar ? asset($comment->user()->avatar) : asset('app_assets/img/blog-comment1.png') ?>" alt="#"/>
                 <div class="homec-blog-comments__detail">
                     <h4 class="homec-blog-comments__title">
-                        میشل هولدر <span><i class="fa-solid fa-clock"></i> 1 اسفند 1403 در 11:27 ق.ظ</span>
+                        <?= $comment->user()->first_name ?> <span><i class="fa-solid fa-clock"><?= \Morilog\Jalali\Jalalian::forge($comment->created_at)->format('%A, %d %B %y'); ?></i></span>
                     </h4>
-                    <p class="homec-blog-comments__text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله</p>
+                    <p class="homec-blog-comments__text">
+                        <?= $comment->comment ?>
+                    </p>
                     <a class="homec-blog-comments__button" href="#">پاسخ</a>
                 </div>
             </div>
+            <?php } ?>
 
-            <div class="homec-blog-comments__single homec-blog-comments__single--reply mg-top-30">
+            <!-- <div class="homec-blog-comments__single homec-blog-comments__single--reply mg-top-30">
                 <img src="<?= asset('app_assets/img/blog-comment2.png') ?>" alt="#" />
                 <div class="homec-blog-comments__detail">
                     <h4 class="homec-blog-comments__title">
@@ -24,7 +28,6 @@
                     <a class="homec-blog-comments__button" href="#">پاسخ</a>
                 </div>
             </div>
-
             <div class="homec-blog-comments__single mg-top-30">
                 <img src="<?= asset('app_assets/img/blog-comment3.png') ?>" alt="#" />
                 <div class="homec-blog-comments__detail">
@@ -46,6 +49,10 @@
                     <a class="homec-blog-comments__button" href="#">پاسخ</a>
                 </div>
             </div>
+
+            -->
+
+
         </div>
     </div>
 </div>
@@ -58,12 +65,14 @@
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-12">
                     <div class="form-group homec-form-input">
-                        <input class="ecom-wc__form-input" type="text" name="f_name" placeholder="میشل هولدر" required="required" />
+                        <input class="ecom-wc__form-input" type="text" name="f_name" placeholder="میشل هولدر"
+                               required="required"/>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-12">
                     <div class="form-group homec-form-input">
-                        <input class="ecom-wc__form-input" type="email" name="f_name" placeholder="ایمیل شما" required="required" />
+                        <input class="ecom-wc__form-input" type="email" name="f_name" placeholder="ایمیل شما"
+                               required="required"/>
                     </div>
                 </div>
                 <div class="col-12">
